c41bd20f0e5a5c421e5a23b42d1bb0f6d564ad7d
万里涛 <wanlitao_3055601@outlook.com>
2017-01-06 20:15:30 +0800

    finish Rest Post, Put, Delete Functions

--- src/FCP.Web.Api.Client/IRestApiClient.cs
+++ src/FCP.Web.Api.Client/IRestApiClient.cs
@@ -25,12 +25,137 @@ namespace FCP.Web.Api.Client
         #endregion
 
         #region Post
+
+        #region Post Empty
+        Task<RestApiResult> PostEmptyAsync(string requestUrl);
+
+        Task<RestApiResult> PostEmptyAsync(Uri requestUri);
+
+        Task<RestApiResult> PostEmptyAsync(string requestUrl, CancellationToken cancellationToken);
+
+        Task<RestApiResult> PostEmptyAsync(Uri requestUri, CancellationToken cancellationToken);
+
+        Task<RestApiResult<T>> PostEmptyAsync<T>(string requestUrl);
+
+        Task<RestApiResult<T>> PostEmptyAsync<T>(Uri requestUri);
+
+        Task<RestApiResult<T>> PostEmptyAsync<T>(string requestUrl, CancellationToken cancellationToken);
+
+        Task<RestApiResult<T>> PostEmptyAsync<T>(Uri requestUri, CancellationToken cancellationToken);
+        #endregion
+
+        #region Post Json String
+        Task<RestApiResult> PostJsonAsync(string requestUrl, string requestJson);
+
+        Task<RestApiResult> PostJsonAsync(Uri requestUri, string requestJson);
+
+        Task<RestApiResult> PostJsonAsync(string requestUrl, string requestJson, CancellationToken cancellationToken);
+
+        Task<RestApiResult> PostJsonAsync(Uri requestUri, string requestJson, CancellationToken cancellationToken);
+
+        Task<RestApiResult<T>> PostJsonAsync<T>(string requestUrl, string requestJson);
+
+        Task<RestApiResult<T>> PostJsonAsync<T>(Uri requestUri, string requestJson);
+
+        Task<RestApiResult<T>> PostJsonAsync<T>(string requestUrl, string requestJson, CancellationToken cancellationToken);
+
+        Task<RestApiResult<T>> PostJsonAsync<T>(Uri requestUri, string requestJson, CancellationToken cancellationToken);
+        #endregion
+
+        #region Post Data AsJson
+        Task<RestApiResult> PostAsJsonAsync<T>(string requestUrl, T requestData);
+
+        Task<RestApiResult> PostAsJsonAsync<T>(Uri requestUri, T requestData);
+
+        Task<RestApiResult> PostAsJsonAsync<T>(string requestUrl, T requestData, CancellationToken cancellationToken);
+
+        Task<RestApiResult> PostAsJsonAsync<T>(Uri requestUri, T requestData, CancellationToken cancellationToken);
+
+        Task<RestApiResult<TResult>> PostAsJsonAsync<TRequest, TResult>(string requestUrl, TRequest requestData);
+
+        Task<RestApiResult<TResult>> PostAsJsonAsync<TRequest, TResult>(Uri requestUri, TRequest requestData);
+
+        Task<RestApiResult<TResult>> PostAsJsonAsync<TRequest, TResult>(string requestUrl, TRequest requestData, CancellationToken cancellationToken);
+
+        Task<RestApiResult<TResult>> PostAsJsonAsync<TRequest, TResult>(Uri requestUri, TRequest requestData, CancellationToken cancellationToken);
+        #endregion
+
         #endregion
 
         #region Put
+
+        #region Put Empty
+        Task<RestApiResult> PutEmptyAsync(string requestUrl);
+
+        Task<RestApiResult> PutEmptyAsync(Uri requestUri);
+
+        Task<RestApiResult> PutEmptyAsync(string requestUrl, CancellationToken cancellationToken);
+
+        Task<RestApiResult> PutEmptyAsync(Uri requestUri, CancellationToken cancellationToken);
+
+        Task<RestApiResult<T>> PutEmptyAsync<T>(string requestUrl);
+
+        Task<RestApiResult<T>> PutEmptyAsync<T>(Uri requestUri);
+
+        Task<RestApiResult<T>> PutEmptyAsync<T>(string requestUrl, CancellationToken cancellationToken);
+
+        Task<RestApiResult<T>> PutEmptyAsync<T>(Uri requestUri, CancellationToken cancellationToken);
+        #endregion
+
+        #region Put Json String
+        Task<RestApiResult> PutJsonAsync(string requestUrl, string requestJson);
+
+        Task<RestApiResult> PutJsonAsync(Uri requestUri, string requestJson);
+
+        Task<RestApiResult> PutJsonAsync(string requestUrl, string requestJson, CancellationToken cancellationToken);
+
+        Task<RestApiResult> PutJsonAsync(Uri requestUri, string requestJson, CancellationToken cancellationToken);
+
+        Task<RestApiResult<T>> PutJsonAsync<T>(string requestUrl, string requestJson);
+
+        Task<RestApiResult<T>> PutJsonAsync<T>(Uri requestUri, string requestJson);
+
+        Task<RestApiResult<T>> PutJsonAsync<T>(string requestUrl, string requestJson, CancellationToken cancellationToken);
+
+        Task<RestApiResult<T>> PutJsonAsync<T>(Uri requestUri, string requestJson, CancellationToken cancellationToken);
+        #endregion
+
+        #region Put Data AsJson
+        Task<RestApiResult> PutAsJsonAsync<T>(string requestUrl, T requestData);
+
+        Task<RestApiResult> PutAsJsonAsync<T>(Uri requestUri, T requestData);
+
+        Task<RestApiResult> PutAsJsonAsync<T>(string requestUrl, T requestData, CancellationToken cancellationToken);
+
+        Task<RestApiResult> PutAsJsonAsync<T>(Uri requestUri, T requestData, CancellationToken cancellationToken);
+
+        Task<RestApiResult<TResult>> PutAsJsonAsync<TRequest, TResult>(string requestUrl, TRequest requestData);
+
+        Task<RestApiResult<TResult>> PutAsJsonAsync<TRequest, TResult>(Uri requestUri, TRequest requestData);
+
+        Task<RestApiResult<TResult>> PutAsJsonAsync<TRequest, TResult>(string requestUrl, TRequest requestData, CancellationToken cancellationToken);
+
+        Task<RestApiResult<TResult>> PutAsJsonAsync<TRequest, TResult>(Uri requestUri, TRequest requestData, CancellationToken cancellationToken);
+        #endregion
+
         #endregion
 
         #region Delete
+        Task<RestApiResult> DeleteAsync(string requestUrl);
+
+        Task<RestApiResult> DeleteAsync(Uri requestUri);
+
+        Task<RestApiResult> DeleteAsync(string requestUrl, CancellationToken cancellationToken);
+
+        Task<RestApiResult> DeleteAsync(Uri requestUri, CancellationToken cancellationToken);
+
+        Task<RestApiResult<T>> DeleteAsync<T>(string requestUrl);
+
+        Task<RestApiResult<T>> DeleteAsync<T>(Uri requestUri);
+
+        Task<RestApiResult<T>> DeleteAsync<T>(string requestUrl, CancellationToken cancellationToken);
+
+        Task<RestApiResult<T>> DeleteAsync<T>(Uri requestUri, CancellationToken cancellationToken);
         #endregion
     }
 }
--- src/FCP.Web.Api.Client/RestApiClient.cs
+++ src/FCP.Web.Api.Client/RestApiClient.cs
@@ -92,6 +92,308 @@ namespace FCP.Web.Api.Client
         }
         #endregion
 
+        #region Post
+
+        #region Post Empty
+        public Task<RestApiResult> PostEmptyAsync(string requestUrl)
+        {
+            return PostEmptyAsync(requestUrl.ToUri());            
+        }
+
+        public Task<RestApiResult> PostEmptyAsync(Uri requestUri)
+        {
+            return PostEmptyAsync(requestUri, CancellationToken.None);
+        }
+
+        public Task<RestApiResult> PostEmptyAsync(string requestUrl, CancellationToken cancellationToken)
+        {
+            return PostEmptyAsync(requestUrl.ToUri(), cancellationToken);
+        }
+
+        public Task<RestApiResult> PostEmptyAsync(Uri requestUri, CancellationToken cancellationToken)
+        {
+            return SendAsync(new RestApiEmptyRequest(HttpMethod.Post, requestUri), cancellationToken);
+        }
+
+        public Task<RestApiResult<T>> PostEmptyAsync<T>(string requestUrl)
+        {
+            return PostEmptyAsync<T>(requestUrl.ToUri());
+        }
+
+        public Task<RestApiResult<T>> PostEmptyAsync<T>(Uri requestUri)
+        {
+            return PostEmptyAsync<T>(requestUri, CancellationToken.None);
+        }
+
+        public Task<RestApiResult<T>> PostEmptyAsync<T>(string requestUrl, CancellationToken cancellationToken)
+        {
+            return PostEmptyAsync<T>(requestUrl.ToUri(), cancellationToken);
+        }
+
+        public Task<RestApiResult<T>> PostEmptyAsync<T>(Uri requestUri, CancellationToken cancellationToken)
+        {
+            return SendAsync<T>(new RestApiEmptyRequest(HttpMethod.Post, requestUri), cancellationToken);
+        }
+        #endregion
+
+        #region Post Json String
+        public Task<RestApiResult> PostJsonAsync(string requestUrl, string requestJson)
+        {
+            return PostJsonAsync(requestUrl.ToUri(), requestJson);
+        }
+
+        public Task<RestApiResult> PostJsonAsync(Uri requestUri, string requestJson)
+        {
+            return PostJsonAsync(requestUri, requestJson, CancellationToken.None);
+        }
+
+        public Task<RestApiResult> PostJsonAsync(string requestUrl, string requestJson, CancellationToken cancellationToken)
+        {
+            return PostJsonAsync(requestUrl.ToUri(), requestJson, cancellationToken);
+        }
+
+        public Task<RestApiResult> PostJsonAsync(Uri requestUri, string requestJson, CancellationToken cancellationToken)
+        {
+            return SendAsync(new RestApiJsonRequest(HttpMethod.Post, requestUri) { Json = requestJson }, cancellationToken);
+        }
+
+        public Task<RestApiResult<T>> PostJsonAsync<T>(string requestUrl, string requestJson)
+        {
+            return PostJsonAsync<T>(requestUrl.ToUri(), requestJson);
+        }
+
+        public Task<RestApiResult<T>> PostJsonAsync<T>(Uri requestUri, string requestJson)
+        {
+            return PostJsonAsync<T>(requestUri, requestJson, CancellationToken.None);
+        }
+
+        public Task<RestApiResult<T>> PostJsonAsync<T>(string requestUrl, string requestJson, CancellationToken cancellationToken)
+        {
+            return PostJsonAsync<T>(requestUrl.ToUri(), requestJson, cancellationToken);
+        }
+
+        public Task<RestApiResult<T>> PostJsonAsync<T>(Uri requestUri, string requestJson, CancellationToken cancellationToken)
+        {
+            return SendAsync<T>(new RestApiJsonRequest(HttpMethod.Post, requestUri) { Json = requestJson }, cancellationToken);
+        }
+        #endregion
+
+        #region Post Data AsJson
+        public Task<RestApiResult> PostAsJsonAsync<T>(string requestUrl, T requestData)
+        {
+            return PostAsJsonAsync(requestUrl.ToUri(), requestData);
+        }
+
+        public Task<RestApiResult> PostAsJsonAsync<T>(Uri requestUri, T requestData)
+        {
+            return PostAsJsonAsync(requestUri, requestData, CancellationToken.None);
+        }
+
+        public Task<RestApiResult> PostAsJsonAsync<T>(string requestUrl, T requestData, CancellationToken cancellationToken)
+        {
+            return PostAsJsonAsync(requestUrl.ToUri(), requestData, cancellationToken);
+        }
+
+        public Task<RestApiResult> PostAsJsonAsync<T>(Uri requestUri, T requestData, CancellationToken cancellationToken)
+        {
+            return SendAsync(new RestApiJsonRequest<T>(HttpMethod.Post, requestUri) { Data = requestData }, cancellationToken);
+        }
+
+        public Task<RestApiResult<TResult>> PostAsJsonAsync<TRequest, TResult>(string requestUrl, TRequest requestData)
+        {
+            return PostAsJsonAsync<TRequest, TResult>(requestUrl.ToUri(), requestData);
+        }
+
+        public Task<RestApiResult<TResult>> PostAsJsonAsync<TRequest, TResult>(Uri requestUri, TRequest requestData)
+        {
+            return PostAsJsonAsync<TRequest, TResult>(requestUri, requestData, CancellationToken.None);
+        }
+
+        public Task<RestApiResult<TResult>> PostAsJsonAsync<TRequest, TResult>(string requestUrl, TRequest requestData, CancellationToken cancellationToken)
+        {
+            return PostAsJsonAsync<TRequest, TResult>(requestUrl.ToUri(), requestData, cancellationToken);
+        }
+
+        public Task<RestApiResult<TResult>> PostAsJsonAsync<TRequest, TResult>(Uri requestUri, TRequest requestData, CancellationToken cancellationToken)
+        {
+            return SendAsync<TResult>(new RestApiJsonRequest<TRequest>(HttpMethod.Post, requestUri) { Data = requestData }, cancellationToken);
+        }
+        #endregion
+
+        #endregion
+
+        #region Put
+
+        #region Put Empty
+        public Task<RestApiResult> PutEmptyAsync(string requestUrl)
+        {
+            return PutEmptyAsync(requestUrl.ToUri());
+        }
+
+        public Task<RestApiResult> PutEmptyAsync(Uri requestUri)
+        {
+            return PutEmptyAsync(requestUri, CancellationToken.None);
+        }
+
+        public Task<RestApiResult> PutEmptyAsync(string requestUrl, CancellationToken cancellationToken)
+        {
+            return PutEmptyAsync(requestUrl.ToUri(), cancellationToken);
+        }
+
+        public Task<RestApiResult> PutEmptyAsync(Uri requestUri, CancellationToken cancellationToken)
+        {
+            return SendAsync(new RestApiEmptyRequest(HttpMethod.Put, requestUri), cancellationToken);
+        }
+
+        public Task<RestApiResult<T>> PutEmptyAsync<T>(string requestUrl)
+        {
+            return PutEmptyAsync<T>(requestUrl.ToUri());
+        }
+
+        public Task<RestApiResult<T>> PutEmptyAsync<T>(Uri requestUri)
+        {
+            return PutEmptyAsync<T>(requestUri, CancellationToken.None);
+        }
+
+        public Task<RestApiResult<T>> PutEmptyAsync<T>(string requestUrl, CancellationToken cancellationToken)
+        {
+            return PutEmptyAsync<T>(requestUrl.ToUri(), cancellationToken);
+        }
+
+        public Task<RestApiResult<T>> PutEmptyAsync<T>(Uri requestUri, CancellationToken cancellationToken)
+        {
+            return SendAsync<T>(new RestApiEmptyRequest(HttpMethod.Put, requestUri), cancellationToken);
+        }
+        #endregion
+
+        #region Put Json String
+        public Task<RestApiResult> PutJsonAsync(string requestUrl, string requestJson)
+        {
+            return PutJsonAsync(requestUrl.ToUri(), requestJson);
+        }
+
+        public Task<RestApiResult> PutJsonAsync(Uri requestUri, string requestJson)
+        {
+            return PutJsonAsync(requestUri, requestJson, CancellationToken.None);
+        }
+
+        public Task<RestApiResult> PutJsonAsync(string requestUrl, string requestJson, CancellationToken cancellationToken)
+        {
+            return PutJsonAsync(requestUrl.ToUri(), requestJson, cancellationToken);
+        }
+
+        public Task<RestApiResult> PutJsonAsync(Uri requestUri, string requestJson, CancellationToken cancellationToken)
+        {
+            return SendAsync(new RestApiJsonRequest(HttpMethod.Put, requestUri) { Json = requestJson }, cancellationToken);
+        }
+
+        public Task<RestApiResult<T>> PutJsonAsync<T>(string requestUrl, string requestJson)
+        {
+            return PutJsonAsync<T>(requestUrl.ToUri(), requestJson);
+        }
+
+        public Task<RestApiResult<T>> PutJsonAsync<T>(Uri requestUri, string requestJson)
+        {
+            return PutJsonAsync<T>(requestUri, requestJson, CancellationToken.None);
+        }
+
+        public Task<RestApiResult<T>> PutJsonAsync<T>(string requestUrl, string requestJson, CancellationToken cancellationToken)
+        {
+            return PutJsonAsync<T>(requestUrl.ToUri(), requestJson, cancellationToken);
+        }
+
+        public Task<RestApiResult<T>> PutJsonAsync<T>(Uri requestUri, string requestJson, CancellationToken cancellationToken)
+        {
+            return SendAsync<T>(new RestApiJsonRequest(HttpMethod.Put, requestUri) { Json = requestJson }, cancellationToken);
+        }
+        #endregion
+
+        #region Put Data AsJson
+        public Task<RestApiResult> PutAsJsonAsync<T>(string requestUrl, T requestData)
+        {
+            return PutAsJsonAsync(requestUrl.ToUri(), requestData);
+        }
+
+        public Task<RestApiResult> PutAsJsonAsync<T>(Uri requestUri, T requestData)
+        {
+            return PutAsJsonAsync(requestUri, requestData, CancellationToken.None);
+        }
+
+        public Task<RestApiResult> PutAsJsonAsync<T>(string requestUrl, T requestData, CancellationToken cancellationToken)
+        {
+            return PutAsJsonAsync(requestUrl.ToUri(), requestData, cancellationToken);
+        }
+
+        public Task<RestApiResult> PutAsJsonAsync<T>(Uri requestUri, T requestData, CancellationToken cancellationToken)
+        {
+            return SendAsync(new RestApiJsonRequest<T>(HttpMethod.Put, requestUri) { Data = requestData }, cancellationToken);
+        }
+
+        public Task<RestApiResult<TResult>> PutAsJsonAsync<TRequest, TResult>(string requestUrl, TRequest requestData)
+        {
+            return PutAsJsonAsync<TRequest, TResult>(requestUrl.ToUri(), requestData);
+        }
+
+        public Task<RestApiResult<TResult>> PutAsJsonAsync<TRequest, TResult>(Uri requestUri, TRequest requestData)
+        {
+            return PutAsJsonAsync<TRequest, TResult>(requestUri, requestData, CancellationToken.None);
+        }
+
+        public Task<RestApiResult<TResult>> PutAsJsonAsync<TRequest, TResult>(string requestUrl, TRequest requestData, CancellationToken cancellationToken)
+        {
+            return PutAsJsonAsync<TRequest, TResult>(requestUrl.ToUri(), requestData, cancellationToken);
+        }
+
+        public Task<RestApiResult<TResult>> PutAsJsonAsync<TRequest, TResult>(Uri requestUri, TRequest requestData, CancellationToken cancellationToken)
+        {
+            return SendAsync<TResult>(new RestApiJsonRequest<TRequest>(HttpMethod.Put, requestUri) { Data = requestData }, cancellationToken);
+        }
+        #endregion
+
+        #endregion
+
+        #region Delete
+        public Task<RestApiResult> DeleteAsync(string requestUrl)
+        {
+            return DeleteAsync(requestUrl.ToUri());
+        }
+
+        public Task<RestApiResult> DeleteAsync(Uri requestUri)
+        {
+            return DeleteAsync(requestUri, CancellationToken.None);
+        }
+
+        public Task<RestApiResult> DeleteAsync(string requestUrl, CancellationToken cancellationToken)
+        {
+            return DeleteAsync(requestUrl.ToUri(), cancellationToken);
+        }
+
+        public Task<RestApiResult> DeleteAsync(Uri requestUri, CancellationToken cancellationToken)
+        {
+            return SendAsync(new RestApiEmptyRequest(HttpMethod.Delete, requestUri), cancellationToken);
+        }
+
+        public Task<RestApiResult<T>> DeleteAsync<T>(string requestUrl)
+        {
+            return DeleteAsync<T>(requestUrl.ToUri());
+        }
+
+        public Task<RestApiResult<T>> DeleteAsync<T>(Uri requestUri)
+        {
+            return DeleteAsync<T>(requestUri, CancellationToken.None);
+        }
+
+        public Task<RestApiResult<T>> DeleteAsync<T>(string requestUrl, CancellationToken cancellationToken)
+        {
+            return DeleteAsync<T>(requestUrl.ToUri(), cancellationToken);
+        }
+
+        public Task<RestApiResult<T>> DeleteAsync<T>(Uri requestUri, CancellationToken cancellationToken)
+        {
+            return SendAsync<T>(new RestApiEmptyRequest(HttpMethod.Delete, requestUri), cancellationToken);
+        }
+        #endregion
+
         #region Send
         protected async Task<RestApiResult> SendAsync(RestApiRequest request, CancellationToken cancellationToken)
         {
