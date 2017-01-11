5d645c6efeab6c9cff7e761cb0ffb5f374d860f7
万里涛 <wanlitao_3055601@outlook.com>
2017-01-11 18:43:46 +0800

    RestApi Client add UpdateByKey extension

--- src/FCP.Web.Api.Client/Extensions/RestApiClientCRUDExtensions.cs
+++ src/FCP.Web.Api.Client/Extensions/RestApiClientCRUDExtensions.cs
@@ -10,6 +10,9 @@ namespace FCP.Web.Api.Client
         #region Helper Functions
         internal static Uri BuildByKeyRequestUri<TKey>(Uri requestUri, TKey keyParam)
         {
+            if (keyParam == null)
+                throw new ArgumentNullException(nameof(keyParam));
+
             var uriBuilder = new FluentUriBuilder().FromUri(requestUri.ToAbsolute())
                 .SegmentParam(TypeHelper.parseString(keyParam));
 
@@ -37,12 +40,37 @@ namespace FCP.Web.Api.Client
         public static Task<RestApiResult<TResult>> GetByKeyAsync<TKey, TResult>(this IRestApiClient restClient, Uri requestUri,
             TKey keyParam, CancellationToken cancellationToken)
         {
-            if (keyParam == null)
-                throw new ArgumentNullException(nameof(keyParam));
-
             var fullRequestUri = BuildByKeyRequestUri(requestUri, keyParam);
 
             return restClient.GetAsync<TResult>(fullRequestUri, cancellationToken);
+        }
+        #endregion
+
+        #region UpdateByKey
+        public static Task<RestApiResult<TResult>> UpdateByKeyAsync<TKey, TUpdate, TResult>(this IRestApiClient restClient, string requestUrl,
+            TKey keyParam, TUpdate updateParam)
+        {
+            return UpdateByKeyAsync<TKey, TUpdate, TResult>(restClient, requestUrl.ToUri(), keyParam, updateParam);
+        }
+
+        public static Task<RestApiResult<TResult>> UpdateByKeyAsync<TKey, TUpdate, TResult>(this IRestApiClient restClient, Uri requestUri,
+            TKey keyParam, TUpdate updateParam)
+        {
+            return UpdateByKeyAsync<TKey, TUpdate, TResult>(restClient, requestUri, keyParam, updateParam, CancellationToken.None);
+        }
+
+        public static Task<RestApiResult<TResult>> UpdateByKeyAsync<TKey, TUpdate, TResult>(this IRestApiClient restClient, string requestUrl,
+            TKey keyParam, TUpdate updateParam, CancellationToken cancellationToken)
+        {
+            return UpdateByKeyAsync<TKey, TUpdate, TResult>(restClient, requestUrl.ToUri(), keyParam, updateParam, cancellationToken);
+        }
+
+        public static Task<RestApiResult<TResult>> UpdateByKeyAsync<TKey, TUpdate, TResult>(this IRestApiClient restClient, Uri requestUri,
+           TKey keyParam, TUpdate updateParam, CancellationToken cancellationToken)
+        {
+            var fullRequestUri = BuildByKeyRequestUri(requestUri, keyParam);
+
+            return restClient.PutAsJsonAsync<TUpdate, TResult>(fullRequestUri, updateParam, cancellationToken);
         }
         #endregion
 
@@ -66,9 +94,6 @@ namespace FCP.Web.Api.Client
         public static Task<RestApiResult<TResult>> DeleteByKeyAsync<TKey, TResult>(this IRestApiClient restClient, Uri requestUri,
             TKey keyParam, CancellationToken cancellationToken)
         {
-            if (keyParam == null)
-                throw new ArgumentNullException(nameof(keyParam));
-
             var fullRequestUri = BuildByKeyRequestUri(requestUri, keyParam);
 
             return restClient.DeleteAsync<TResult>(fullRequestUri, cancellationToken);
